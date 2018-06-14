889beecf0faff01cac66bf10ee1da22ee20a2b50
Dhruv Bhavsar <dhruvhbhavsar@gmail.com>
2018-06-14 10:57:04 -0400

    Initial Unit Test Success

--- Brownbag.Web/ClientApp/src/app/blog-view/blog-view.component.spec.ts
+++ Brownbag.Web/ClientApp/src/app/blog-view/blog-view.component.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { HttpClientModule, HttpBackend } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdvGrowlModule, AdvGrowlService } from 'primeng-advanced-growl';
@@ -14,6 +15,7 @@ import { PaginatorModule } from 'primeng/paginator';
 import { RatingModule } from 'primeng/rating';
 import { TableModule } from 'primeng/table';
 import { Observable } from 'rxjs/Observable';
+import { GridRestCallsBaseService } from '../../shared/service/grid-base-rest.service';
 import { GridComponentBaseService } from '../../shared/service/grid-component-base.service';
 import { LookupsService } from '../../shared/service/lookups.service';
 import { BlogViewComponent } from './blog-view.component';
@@ -24,6 +26,10 @@ import { BlogViewService } from './blog-view.service';
 describe('BlogViewComponent', () => {
   let component: BlogViewComponent;
   let fixture: ComponentFixture<BlogViewComponent>;
+  const mockLookupService = jasmine.createSpyObj('mockLookupService', ['getBlogs']);
+  const mockBlogViewService = jasmine.createSpyObj('mockBlogViewService', ['getGrid']);
+  const mockGridRestCallsBaseService = jasmine.createSpyObj('mockGridRestCallsBaseService', ['getGrid']);
+  const mockAdvGrowlService = jasmine.createSpyObj('mockAdvGrowlService', ['createSuccessMessage']);
   let LookupsServiceStub: Partial<LookupsService>;
   let BlogViewServiceStub: Partial<BlogViewService>;
   const AfrTypesResponse: Brownbag.Web.Models.LookupViewModel[] = [{ 'ID': 1, 'Value': 'google.com' }, { 'ID': 2, 'Value': 'apple.com' }];
@@ -38,7 +44,12 @@ describe('BlogViewComponent', () => {
       return Observable.of(blogViewGridData);
     }
   };
+  // let injector: TestBed;
+  // let httpMock: HttpTestingController;
   beforeEach(async(() => {
+    mockLookupService.getBlogs.and.returnValue(Observable.of(AfrTypesResponse));
+    mockBlogViewService.getGrid.and.returnValue(Observable.of(blogViewGridData));
+    mockGridRestCallsBaseService.getGrid.and.returnValue(Observable.of(blogViewGridData));
     TestBed.configureTestingModule({
       declarations: [BlogViewComponent],
       imports: [
@@ -57,15 +68,18 @@ describe('BlogViewComponent', () => {
         AdvGrowlModule,
       ],
       providers: [
-        { provide: LookupsService, useValue: LookupsServiceStub },
-        { provide: BlogViewService, useValue: BlogViewServiceStub },
-        { provide: AdvGrowlService, useValue: AdvGrowlService },
-        { provide: GridComponentBaseService, useValue: GridComponentBaseService }
+        { provide: LookupsService, useValue: mockLookupService },
+        { provide: BlogViewService, useValue: mockBlogViewService },
+        { provide: AdvGrowlService, useValue: mockAdvGrowlService },
+        // { provide: GridComponentBaseService, useValue: GridComponentBaseService },
+        { provide: GridRestCallsBaseService, useValue: mockGridRestCallsBaseService }
       ]
       // private lookupsService: LookupsService, gridService: BlogViewService, notificationsService: AdvGrowlService
     })
       .compileComponents();
   }));
+  // injector = getTestBed();
+  // httpMock = injector.get(HttpTestingController);
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BlogViewComponent);
