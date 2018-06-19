a9b1752b5ac8e922f88dd15a94589e2d32b126a5
Dhruv Bhavsar <dhruvb@microsoft.com>
2018-06-19 15:52:55 -0400

    Fix Unit Tests for Blog FK so CI works

--- Brownbag.Web/ClientApp/src/app/blog-fk/blog-fk.component.spec.ts
+++ Brownbag.Web/ClientApp/src/app/blog-fk/blog-fk.component.spec.ts
@@ -1,23 +1,61 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule, HttpBackend } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { AdvGrowlModule, AdvGrowlService } from 'primeng-advanced-growl';
+import { CardModule } from 'primeng/card';
+import { DataGridModule } from 'primeng/datagrid';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { EditorModule } from 'primeng/editor';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { PaginatorModule } from 'primeng/paginator';
+import { RatingModule } from 'primeng/rating';
+import { TableModule } from 'primeng/table';
+import { Observable } from 'rxjs/Observable';
 
-import { BlogComponent } from './blog.component';
+import { BlogFKComponent } from './blog-fk.component';
+import { BlogFKService } from './blog-fk.service';
+import { BlogFKData } from './blog-fk.data';
 
-describe('BlogComponent', () => {
-  let component: BlogComponent;
-  let fixture: ComponentFixture<BlogComponent>;
+describe('BlogFKComponent', () => {
+  let component: BlogFKComponent;
+  let fixture: ComponentFixture<BlogFKComponent>;
+  const mockAdvGrowlService = jasmine.createSpyObj('mockAdvGrowlService', ['createSuccessMessage']);
+  const mockBlogViewService = jasmine.createSpyObj('mockBlogViewService', ['getGrid']);
 
   beforeEach(async(() => {
+    mockBlogViewService.getGrid.and.returnValue(Observable.of(BlogFKData));
     TestBed.configureTestingModule({
-      declarations: [ BlogComponent ]
+      declarations: [BlogFKComponent],
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        DropdownModule,
+        TableModule,
+        DialogModule,
+        PaginatorModule,
+        InputTextareaModule,
+        RatingModule,
+        EditorModule,
+        DataGridModule,
+        CardModule,
+        BrowserAnimationsModule,
+        AdvGrowlModule,
+      ],
+      providers: [
+        { provide: BlogFKService, useValue: mockBlogViewService },
+        { provide: AdvGrowlService, useValue: mockAdvGrowlService },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BlogComponent);
+    fixture = TestBed.createComponent(BlogFKComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -25,4 +63,41 @@ describe('BlogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open Add Blog modal', async(() => {
+    const cell = fixture.debugElement.queryAll(By.css('.ui-table-caption.ui-widget-header.ng-star-inserted .ng-star-inserted button'))[0];
+    cell.nativeElement.click();
+
+    fixture.detectChanges();
+    const modal = fixture.debugElement.queryAll(By.css('.ui-dialog-titlebar.ui-widget-header span'))[0];
+    expect(modal.nativeElement.textContent).toEqual('Add Blog FK');
+  }));
+
+  it('should display a title', async(() => {
+    const titleText = fixture.nativeElement.querySelector('h2').textContent;
+    expect(titleText).toEqual('Blogs FK');
+  }));
+
+  it('should call onRowSelect on row click', async(() => {
+    const spy = spyOn(component, 'onRowSelect');
+    fixture.detectChanges();
+    const cell = fixture.debugElement.queryAll(By.css('.ui-table-tbody .ng-star-inserted'))[0];
+    cell.nativeElement.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(spy).toHaveBeenCalled();
+    });
+  }));
+
+  // it('should open Edit Post modal', async(() => {
+  //   fixture.detectChanges();
+  //   // const cell = fixture.debugElement.queryAll(By.css('.ui-table-tbody .ng-star-inserted'))[0];
+  //   const cell = fixture.debugElement.queryAll(By.css('tr.ng-star-inserted'))[1];
+  //   cell.nativeElement.click();
+  //   fixture.detectChanges();
+
+  //   const modal = fixture.debugElement.queryAll(By.css('#ui-dialog-1-label'))[0];
+  //   expect(modal.nativeElement.textContent).toEqual('Edit Blog');
+  // }));
+
 });
--- Brownbag.Web/ClientApp/src/app/blog-fk/blog-fk.service.spec.ts
+++ Brownbag.Web/ClientApp/src/app/blog-fk/blog-fk.service.spec.ts
@@ -1,16 +1,68 @@
 /* tslint:disable:no-unused-variable */
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { BlogService } from './blog.service';
+import { TestBed, async, inject, getTestBed } from '@angular/core/testing';
+import { BlogFKService } from './blog-fk.service';
+import { HttpClient } from '@angular/common/http';
+import { GridRestCallsBaseService } from '../../shared/service/grid-base-rest.service';
 
-describe('Service: Blog', () => {
+describe('Service: BlogFK', () => {
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BlogService]
+      imports: [HttpClientTestingModule],
+      providers: [BlogFKService, GridRestCallsBaseService]
     });
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it('should ...', inject([BlogService], (service: BlogService) => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+  // tslint:disable-next-line:max-line-length
+  it('should match endpoint for Blog getGrid', inject([BlogFKService, GridRestCallsBaseService], (service: BlogFKService, base: GridRestCallsBaseService) => {
     expect(service).toBeTruthy();
+    const page = 0;
+    const rows = 10;
+    const searchQuery = undefined;
+        service.getGrid(page, rows, searchQuery).subscribe();
+
+        const testUri = service.baseUrl + service.endpoint + `?currentPage=0&rows=10`;
+        const reqMatch = {
+            method: 'Get',
+            url: testUri
+        };
+
+        httpMock.expectOne(reqMatch);
+  }));
+  // tslint:disable-next-line:max-line-length
+  it('should match endpoint for Blog getGridItemDetails', inject([BlogFKService, GridRestCallsBaseService], (service: BlogFKService, base: GridRestCallsBaseService) => {
+    expect(service).toBeTruthy();
+    const itemId = '10';
+        service.getGridItemDetails(itemId).subscribe();
+
+        const testUri = service.baseUrl + service.endpoint + itemId;
+        const reqMatch = {
+            method: 'Get',
+            url: testUri
+        };
+
+        httpMock.expectOne(reqMatch);
+  }));
+  // tslint:disable-next-line:max-line-length
+  it('should match endpoint for Blog updateGridItem', inject([BlogFKService, GridRestCallsBaseService], (service: BlogFKService, base: GridRestCallsBaseService) => {
+    expect(service).toBeTruthy();
+    const fakeEntity = {};
+        service.updateGridItem(fakeEntity).subscribe();
+
+        const testUri = service.baseUrl + service.endpoint;
+        const reqMatch = {
+            method: 'Put',
+            url: testUri,
+        };
+
+        httpMock.expectOne(reqMatch);
   }));
 });
